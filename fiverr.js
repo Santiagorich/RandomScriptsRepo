608ae0d27aef424210d690db69638f753662a86f
ValRab <Santiagorecoba@hotmail.es>
2022-09-18 10:59:03 -0300

    Update fiverr.js

--- fiverr.js
+++ fiverr.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.fiverr.com/users/*/requests*
+// @match        https://*.fiverr.com/users/*/requests*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fiverr.com
 // @grant        none
 // ==/UserScript==
